78af42eb538d75ac1f5fb205e80089411f280fd3
maschulze <maschulze@users.noreply.github.com>
2025-02-19 16:35:19 +0100

    updated

--- build/build_android.sh
+++ build/build_android.sh
@@ -13,3 +13,18 @@ cd output
 git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH="$(pwd)/depot_tools:$PATH"
 git clone -b $CHROMIUM --depth=2 https://chromium.googlesource.com/chromium/src
+
+echo 'solutions = [
+      {
+        "name": "src",
+        "url": "https://chromium.googlesource.com/chromium/src.git",
+        "managed": False,
+        "custom_deps": {},
+        "custom_vars": {},
+      },
+]
+target_os=["android"]' > .gclient
+
+cat .gclient
+
+gclient sync --no-history --nohooks
