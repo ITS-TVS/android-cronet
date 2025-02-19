43c6718ce4c6d6e8f285f044b7c9f2a872222529
maschulze <maschulze@users.noreply.github.com>
2025-02-19 16:30:34 +0100

    updated

--- build/build_android.sh
+++ build/build_android.sh
@@ -4,4 +4,12 @@ set -e
 
 echo "Hello Build!"
 
-ls -al
+export CHROMIUM=133.0.6943.98
+
+mkdir -p output
+rm -rf output/*
+cd output
+
+git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH="$(pwd)/depot_tools:$PATH"
+git clone -b $CHROMIUM --depth=2 https://chromium.googlesource.com/chromium/src
