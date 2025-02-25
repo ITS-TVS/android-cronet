4e582378f198709afc18a5d57ae3791fce1a99a7
ahagelstein <ahagelstein@users.noreply.github.com>
2025-02-25 21:28:03 +0100

    Update build_android.sh

--- build/build_android.sh
+++ build/build_android.sh
@@ -6,22 +6,25 @@ export CHROMIUM=133.0.6943.98
 
 echo "Build cronet v$CHROMIUM"
 
-mkdir -p output
-rm -rf output/*
-cd output
+mkdir ~/chromium && cd ~/chromium
+fetch --nohooks --no-history chromium
 
-git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH="$(pwd)/depot_tools:$PATH"
-git clone -b $CHROMIUM --depth=2 https://chromium.googlesource.com/chromium/src
+# mkdir -p output
+# rm -rf output/*
+# cd output
+
+# git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+# export PATH="$(pwd)/depot_tools:$PATH"
+# git clone -b $CHROMIUM --depth=2 https://chromium.googlesource.com/chromium/src
 
 echo "directory"
 ls -l
 
-gclient config "https://chromium.googlesource.com/chromium/src.git"
-echo 'target_os = ["android"]' >> .gclient
+# gclient config "https://chromium.googlesource.com/chromium/src.git"
+# echo 'target_os = ["android"]' >> .gclient
 
-echo "start gclient"
-gclient sync --no-history --nohooks
+# echo "start gclient"
+# gclient sync --no-history --nohooks
 
 # echo 'solutions = [
 #       {
