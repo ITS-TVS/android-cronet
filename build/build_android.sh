a3f135915366bd09cfedb7f9da0f21bb72105032
ahagelstein <ahagelstein@users.noreply.github.com>
2025-02-25 20:58:06 +0100

    Update build_android.sh

--- build/build_android.sh
+++ build/build_android.sh
@@ -10,10 +10,11 @@ mkdir -p output
 rm -rf output/*
 cd output
 
-git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH="$(pwd)/depot_tools:$PATH"
-git clone -b $CHROMIUM --depth=2 https://chromium.googlesource.com/chromium/src
+# git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+# export PATH="$(pwd)/depot_tools:$PATH"
+# git clone -b $CHROMIUM --depth=2 https://chromium.googlesource.com/chromium/src
 
+echo $(pwd)
 ls -l
 
 echo 'solutions = [
